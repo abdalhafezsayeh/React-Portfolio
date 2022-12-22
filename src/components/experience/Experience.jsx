73a923a85da12e0265b436a6a8ca64a5dcb7a804
abdalhafezsayeh <abdalahfezsayeh1997@gmail.com>
2022-12-22 15:00:02 +0200

    LayOut My Experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import "./experience.css";
-
 // import icons
-import { AiFillHtml5 } from "react-icons/ai";
+import { AiFillHtml5, AiOutlineGithub } from "react-icons/ai";
+import { DiCss3 } from "react-icons/di";
+import { FaSass, FaReact } from "react-icons/fa";
+import {
+  TbBrandBootstrap,
+  TbBrandJavascript,
+  TbBrandReactNative,
+  TbBrandAngular,
+} from "react-icons/tb";
+import {
+  SiRedux,
+  SiNodedotjs,
+  SiMongodb,
+  SiSocketdotio,
+  SiExpress,
+  SiMaterialui,
+  SiTypescript,
+} from "react-icons/si";
+
+
+
 
 function Experience() {
   return (
@@ -14,11 +33,10 @@ function Experience() {
       {/* Start LayOut  */}
       <div className="layOut_ex">
         <Row>
-
           {/* Start Skills Front-End  */}
           <Col md={12} lg={6}>
             <div className="box_track">
-              <h4 className="text-center title_box_ex">Frontend Development</h4>
+              {/* <h4 className="text-center title_box_ex">Front-End Development</h4> */}
               <Row>
                 <Col>
                   <ul className="border_line_right ul_mobil">
@@ -26,7 +44,28 @@ function Experience() {
                       <span className="icon_ex">
                         <AiFillHtml5 />
                       </span>
-                      <span className="lang_ex">HTML</span>
+                      <span className="lang_ex">HTML/5</span>
+                      <p className="des_ex">Experienced</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <DiCss3 />
+                      </span>
+                      <span className="lang_ex">CSS/3</span>
+                      <p className="des_ex">Experienced</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <FaSass />
+                      </span>
+                      <span className="lang_ex">SASS</span>
+                      <p className="des_ex">Experienced</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <TbBrandBootstrap />
+                      </span>
+                      <span className="lang_ex">R-Bootstrap</span>
                       <p className="des_ex">Experienced</p>
                     </li>
                   </ul>
@@ -36,9 +75,30 @@ function Experience() {
                   <ul className="ul_mobil">
                     <li>
                       <span className="icon_ex">
-                        <AiFillHtml5 />
+                        <TbBrandJavascript />
                       </span>
-                      <span className="lang_ex">HTML</span>
+                      <span className="lang_ex">JavaScript</span>
+                      <p className="des_ex">Experienced</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <FaReact />
+                      </span>
+                      <span className="lang_ex">React</span>
+                      <p className="des_ex">Experienced</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <TbBrandReactNative />
+                      </span>
+                      <span className="lang_ex">React Native</span>
+                      <p className="des_ex">Experienced</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <SiRedux />
+                      </span>
+                      <span className="lang_ex">Redux & ToolKit</span>
                       <p className="des_ex">Experienced</p>
                     </li>
                   </ul>
@@ -48,21 +108,40 @@ function Experience() {
           </Col>
           {/* End Skills Front-End  */}
 
-
-
           {/* Start Skills Back-End */}
           <Col md={12} lg={6}>
             <div className="box_track">
-              <h4 className="text-center title_box_ex">Backend Development</h4>
+              {/* <h4 className="text-center title_box_ex">Still Learning Tooll</h4> */}
               <Row>
                 <Col>
                   <ul className="border_line_right ul_mobil">
                     <li>
                       <span className="icon_ex">
-                        <AiFillHtml5 />
+                        <AiOutlineGithub />
                       </span>
-                      <span className="lang_ex">HTML</span>
-                      <p className="des_ex">Experienced</p>
+                      <span className="lang_ex">Git/GitHub</span>
+                      <p className="des_ex">Intermediate</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <TbBrandAngular />
+                      </span>
+                      <span className="lang_ex">Angular</span>
+                      <p className="des_ex">Intermediate</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <SiTypescript />
+                      </span>
+                      <span className="lang_ex">TypeScript</span>
+                      <p className="des_ex">Intermediate</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <SiNodedotjs />
+                      </span>
+                      <span className="lang_ex">Node</span>
+                      <p className="des_ex">Intermediate</p>
                     </li>
                   </ul>
                 </Col>
@@ -71,10 +150,31 @@ function Experience() {
                   <ul className="ul_mobil">
                     <li>
                       <span className="icon_ex">
-                        <AiFillHtml5 />
+                        <SiMongodb />
                       </span>
-                      <span className="lang_ex">HTML</span>
-                      <p className="des_ex">Experienced</p>
+                      <span className="lang_ex">Mongodb</span>
+                      <p className="des_ex">Intermediate</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <SiMaterialui />
+                      </span>
+                      <span className="lang_ex">Material Ui</span>
+                      <p className="des_ex">Basic</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <SiSocketdotio />
+                      </span>
+                      <span className="lang_ex">Socket.Io</span>
+                      <p className="des_ex">Basic</p>
+                    </li>
+                    <li>
+                      <span className="icon_ex">
+                        <SiExpress />
+                      </span>
+                      <span className="lang_ex">Express</span>
+                      <p className="des_ex">Basic</p>
                     </li>
                   </ul>
                 </Col>
@@ -82,7 +182,6 @@ function Experience() {
             </div>
           </Col>
           {/* End Skills Back-End  */}
-
         </Row>
       </div>
       {/* End LayOut  */}
